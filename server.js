03f5d783e16bd1251d946775723a48c469cc07f7
Nei1s <151822072+Nei1s@users.noreply.github.com>
2024-02-21 17:40:53 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.get("/", function(request, response){
-     
-    response.send("</h1>Привет, Октагон!</h1>");
+
+app.get('/dynamic', (req, res) => {
+    const { a, b, c } = req.query;
+
+  if (a === undefined || b === undefined || c === undefined) {
+    res.json({ header: 'Error' });
+    return;
+  }
+
+  const result = (a * b * c) / 3;
+  res.json({ header: 'Calculated', body: result.toString() });
 });
+
+app.get('/static', (req, res) => {
+  res.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+});
+
 app.listen(3000);
