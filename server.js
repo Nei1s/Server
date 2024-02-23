61bc4937cb59dbdbe7b93c7f3dd695b7684a3f66
Nei1s <151822072+Nei1s@users.noreply.github.com>
2024-02-23 16:44:21 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,75 @@
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
 const app = express();
 
-app.get('/dynamic', (req, res) => {
-  const { a, b, c } = req.query;
-
-  if (a == null && isNaN(a) || b == null && isNaN(b) || c == null && isNaN(c)) {
-    res.json({ header: 'Error' });
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "chatbottests",
+  password: ""
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Ошибка подключения к базе данных: ' + err.stack);
     return;
   }
-  else {
-    const result = (a * b * c) / 3;
-    res.json({ header: 'Calculated', body: result.toString() });
+  console.log('Подключено к базе данных с id ' + connection.threadId);
+});
+app.get('/getAllItems', (req, res) => {
+  connection.query('SELECT * FROM items', (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+app.post('/addItem', (req, res) => {
+  const { name, desc } = req.query;
+
+  if (!name || !desc) {
+    res.json({ result: null });
+    return;
   }
-});
 
-app.get('/static', (req, res) => {
-  res.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
-});
+  connection.query('INSERT INTO items (name, desc) VALUES (?, ?)', [name, desc], (error, results) => {
+    if (error) throw error;
 
-app.listen(3000);
+    const newItemId = results.insertId;
+    res.json({ id: newItemId, name, desc });
+  });
+});
+app.post('/deleteItem', (req, res) => {
+  const itemId = parseInt(req.query.id);
+
+  if (isNaN(itemId)) {
+      res.json({ result: null });
+      return;
+  }
+
+  connection.query('DELETE FROM items WHERE id = ?', [itemId], (error, results) => {
+      if (error) throw error;
+
+      const isItemDeleted = results.affectedRows > 0;
+      res.json({ result: isItemDeleted });
+  });
+});
+app.post('/updateItem', (req, res) => {
+  const itemId = parseInt(req.query.id);
+  const { name, desc } = req.query;
+
+  if (isNaN(itemId) || !name || !desc) {
+      res.json({ result: null });
+      return;
+  }
+
+  connection.query('UPDATE items SET name = ?, desc = ? WHERE id = ?', [name, desc, itemId], (error, results) => {
+      if (error) throw error;
+
+      const isItemUpdated = results.changedRows > 0;
+      res.json({ result: isItemUpdated });
+  });
+});
+connection.end(function (err) {
+  if (err) {
+    return console.log("Ошибка: " + err.message);
+  }
+  console.log("Подключение закрыто");
+});
