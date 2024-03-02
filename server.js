b81f57c8652e79da74f0b5a21247418baa1cc47a
Nei1s <151822072+Nei1s@users.noreply.github.com>
2024-03-02 16:25:16 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,15 +4,40 @@ const token = '6851010845:AAH23jdrIg8AORNgqAQzIlw6FKjPnumUSG8';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
+bot.on('text', async msg => {
 
-  bot.sendMessage(chatId, resp);
-});
+  try {
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+      if(msg.text.startsWith('/help')) {
+          
+        const message = "Список команд:\n" +
+        "/help - показать список команд\n" +
+        "/site - получить ссылку на сайт октагона\n" +
+        "/creator - узнать создателя бота";
+        await bot.sendMessage(msg.chat.id, message);
 
-  bot.sendMessage(chatId, 'Привет, октагон!');
+      }
+      else if(msg.text == '/site') {
+
+          await bot.sendMessage(msg.chat.id, `https://students.forus.ru/`);
+
+      }
+      else if(msg.text == '/creator') {
+
+          await bot.sendMessage(msg.chat.id, `Рогожников Данил Александрович`);
+
+      }
+      else {
+
+          await bot.sendMessage(msg.chat.id, msg.text);
+
+      }
+
+  }
+  catch(error) {
+
+      console.log(error);
+
+  }
+
 });
